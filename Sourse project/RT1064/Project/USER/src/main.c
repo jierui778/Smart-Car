3740b6e2345207e30f43e62beeb562557ff0f235
jierui <758418101@qq.com>
2023-09-23 19:21:57 +0800

    修改电机驱动部分代码并增加部分注释

--- Sourse project/RT1064/Project/USER/src/main.c
+++ Sourse project/RT1064/Project/USER/src/main.c
@@ -10,11 +10,11 @@ unsigned int TH;
 static float i=0.0;
 int main(void)
 {
-    clock_init(SYSTEM_CLOCK_600M); // ����ɾ��
+    clock_init(SYSTEM_CLOCK_600M); //
 //    Encoder_Init();
     debug_init();
-//    gpio_init(B9, GPO, GPIO_HIGH, GPO_PUSH_PULL); // ��ʼ�� LED1 ���? Ĭ�ϸߵ�ƽ �������ģ�?
-//    tft180_set_dir(TFT180_PORTAIT);             // ��Ҫ�Ⱥ��� ��Ȼ��ʾ����
+//    gpio_init(B9, GPO, GPIO_HIGH, GPO_PUSH_PULL);
+//    tft180_set_dir(TFT180_PORTAIT);
 //    tft180_init();
 //    Buzzer_Init();
 ////    tft180_show_string(0, 0, "mt9v03x init.");
@@ -24,19 +24,19 @@ int main(void)
 //    while (1)
 //    {
 //        if (mt9v03x_init())
-//            gpio_toggle_level(B9); // ��ת LED ����������? ���� LED ���� ��ʼ����������ƻ����ĺ���?
+//            gpio_toggle_level(B9);
 //        else
 //            break;
-//        system_delay_ms(1000); // ���Ʊ�ʾ�쳣
+//        system_delay_ms(1000);
 //    }
 
     while (1)
     {
 ////////		Image_Compress();
-//////        TH=GetThreshold();					//����ֵ����
+//////        TH=GetThreshold();					
 //////		tft180_show_uint(4,80,TH,3);
 //////		Binarization(TH);
-//////		tft180_displayimage03x((uint8 *)Image_Use, 100, 60); //
+//////		tft180_displayimage03x((uint8 *)Image_Use, 100, 60);
 //        system_delay_ms(10);
 //        pwm_set_duty(SERVO_CH, 800);
 
