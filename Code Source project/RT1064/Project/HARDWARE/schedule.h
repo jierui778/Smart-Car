0d68f2e27d38419982a8cf6cc90e291c2fa3cff8
jierui <758418101@qq.com>
2023-10-08 00:13:26 +0800

    修改CAM文件

--- Code Source project/RT1064/Project/HARDWARE/schedule.h
+++ Code Source project/RT1064/Project/HARDWARE/schedule.h
@@ -19,21 +19,20 @@
 //     interrupt_global_enable(interrupt_state);
 // } // 确保在部分代码块中禁用中断，以确保代码块的原子性
 
-static uint8 Schedule_Counter;//调度器计数器.时基为中断周期，以中断初始化周期为准
-volatile static uint8 Counter_Flag;//调度器计时标志位
+static uint8 Schedule_Counter;      // 调度器计数器.时基为中断周期，以中断初始化周期为准
+volatile static uint8 Counter_Flag; // 调度器计时标志位
 
-//任务
+// 任务
 typedef struct
 {
-    void (*task)(void);//任务执行的函数指针
-    uint32 period;//任务执行的周期，具体时间取决于时基
-    uint32 delay;//任务延时
-    uint8 flag; // 任务是否执行
+    void (*task)(void); // 任务执行的函数指针
+    uint32 period;      // 任务执行的周期，具体时间取决于时基
+    uint32 delay;       // 任务延时
+    uint8 flag;         // 任务是否执行
 } Task_t;
 // your code goes here
 
-
 // void Schedule_Setup(void);
 void Schedule_Run(void);
-void pit_handler(void);
-#endif // SCHEDULER_H
+void pit_handler(void); // 中断更新任务的时基
+#endif                  // SCHEDULER_H
